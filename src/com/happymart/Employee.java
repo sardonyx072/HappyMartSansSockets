bd01fa699ae0d4de1d8bc0ca1e15f4a858723a5a
hoff3539 <hoff3539@OSS327-06.stthomas.edu>
2016-11-14 19:20:01 -0600

    but actually tho

--- src/com/happymart/Employee.java
+++ src/com/happymart/Employee.java
@@ -2,18 +2,39 @@ package com.happymart;
 
 import java.util.UUID;
 
+/**
+ * A class that represents an employee. 
+ * @author hoff3539
+ *
+ */
 public class Employee {
+	/**
+	 * Represents the employee's ID number, used for looking up the employee quickly.
+	 */
 	private UUID id;
+	
+	/**
+	 * Represents the employee's name.
+	 */
 	private String name;
+	
+	/**
+	 * Represents the employee's username. Most usernames should be set with a total of 8 characters: up to 4 letters of the last name and the remaining characters being numbers that create a unique identifier.
+	 */
 	private String user;
+	
+	/**
+	 * Represents the employee's password.
+	 */
 	private String pass;
 	
-	public Employee (UUID id, String name, String user, String pass) {
-		this.id = id;
+	public Employee (String name, String user, String pass) {
+		this.id = UUID.randomUUID();
 		this.name = name;
 		this.user = user;
 		this.pass = pass;
 	}
+	
 	public UUID getID() {
 		return this.id;
 	}
@@ -23,16 +44,16 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getUser() {
+	public String getUsername() {
 		return this.user;
 	}
-	public void setUser(String user) {
+	public void setUsername(String user) {
 		this.user = user;
 	}
-	public String getPass() {
+	public String getPassword() {
 		return this.pass;
 	}
-	public void setPass(String pass) {
+	public void setPassword(String pass) {
 		this.pass = pass;
 	}
 }
